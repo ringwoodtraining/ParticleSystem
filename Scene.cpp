7b25faf5b10b0056384d3413fb6bcffd3396fc66
Nicholas White <107016246+nwhitert@users.noreply.github.com>
2024-05-31 14:51:27 +1000

    Added Scene to project.

---
+++ Scene.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Cisco on 31/05/2024.
+//
+
+#include "Scene.hpp"
+
+namespace ParticleSystem {
+} // ParticleSystem
---
+++ Scene.hpp
@@ -0,0 +1,19 @@
+//
+// Created by Cisco on 31/05/2024.
+//
+
+#pragma once
+
+namespace ParticleSystem
+{
+
+    class Scene
+    {
+    public:
+
+    private:
+
+    };
+
+} // ParticleSystem
+
